3794422c0dd1be5afc13615050d27246f54cff69
Mohammad Tariqul Islam <75881702+tariqul555@users.noreply.github.com>
2023-07-12 21:01:09 +0300

    first commit

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,26 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+const User = g.model('user', {
+  name: g.string().length({min: 2, max: 20}),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedinurl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional(),
+})
+
+const Project = g.model('Project', {
+  title: g.string().length({min: 3}),
+  description: g.string(),
+  image: g.url(),
+  liveSiteurl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User)
+})
+
+export default config({
+  schema: g
+
+})
