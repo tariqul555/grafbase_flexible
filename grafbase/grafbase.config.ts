4c9b4980d50b0e88bde741db8ff80fc429cf3f9e
Mohammad Tariqul Islam <75881702+tariqul555@users.noreply.github.com>
2023-07-12 23:37:28 +0300

    Update grafbase.config.ts

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,6 +1,6 @@
 import { g, auth, config } from '@grafbase/sdk'
 
-const User = g.model('user', {
+const User = g.model('User', {
   name: g.string().length({min: 2, max: 20}),
   email: g.string().unique(),
   avatarUrl: g.url(),
